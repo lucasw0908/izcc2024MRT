17526c3fa6fa1095cca9e0e21dc644047ce6a47c
lucasw0908 <141011290+lucasw0908@users.noreply.github.com>
2024-06-19 22:26:41 +0800

    update

--- flask/app/game_config.py
+++ flask/app/game_config.py
@@ -1,9 +1,9 @@
 import json
-import os
 
-path = os.path.dirname(os.path.abspath(__file__))
+from .config import BASEDIR
 
-GAME_CONFIG = json.load(open(path + "/game_config.json"))
+
+GAME_CONFIG = json.load(open(BASEDIR + "/game_config.json"))
 STATION_POINTS = GAME_CONFIG["station_points"]
 SPECIAL_STATIONS = GAME_CONFIG["special_stations"]
 API_URL = r"https://tdx.transportdata.tw/api/basic/v2/Rail/Metro/StationOfLine/TRTC?%24top=10000&%24format=JSON"
