d9b4c3e8cab1c293488fee3078ae01b26858b689
lucasw0908 <141011290+lucasw0908@users.noreply.github.com>
2024-07-18 02:11:13 +0800

    update

--- flask/app/core/team.py
+++ flask/app/core/team.py
@@ -1,7 +1,7 @@
 from ..game_config import START_STATION
 
 class Team:
-    def __init__(self, name: str, players: list[str]=[], admins: list[str]=[], location: str=None) -> None:
+    def __init__(self, name: str, players: list[str]=[], admins: list[str]=[], location: str | None=None) -> None:
         self.name = name
         self.location = location if location is not None else START_STATION
         self.players = players
